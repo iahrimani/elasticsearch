d5c1c9f9aa112a8b49d05b1615256dcb103f1ba9
d.bobrikov <ahriman@inbox.ru>
2021-05-04 12:48:53 +0300

    Установлен и настроен babenkoivan/elastic-migrations.

---
+++ elastic/migrations/2021_05_04_094424_create_users_index.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use ElasticAdapter\Indices\Mapping;
+use ElasticAdapter\Indices\Settings;
+use ElasticMigrations\Facades\Index;
+use ElasticMigrations\MigrationInterface;
+
+final class CreateUsersIndex implements MigrationInterface
+{
+    /**
+     * Run the migration.
+     */
+    public function up(): void
+    {
+        Index::create('users', function (Mapping $mapping, Settings $settings) {
+            $mapping->text('name');
+            $mapping->keyword('email');
+        });
+    }
+
+    /**
+     * Reverse the migration.
+     */
+    public function down(): void
+    {
+        Index::dropIfExists('users');
+    }
+}
